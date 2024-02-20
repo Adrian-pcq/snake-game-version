0e1b05b6634caed88ca677d96fcf1ddff103cd56
Adrian <adrianqueizan@gmail.com>
2024-02-20 11:47:06 -0500

    give move to snake

--- game/snake.py
+++ game/snake.py
@@ -1,7 +1,7 @@
 from turtle import Turtle
 
 POSITIONS = [(0,0), (-20,0), (-40,0)]
-MOVE = 20
+MOVE = 20 # each segment of the snake has 20 pixels 
 
 class Snake:
     
@@ -31,3 +31,27 @@ class Snake:
         self.segments.clear()
         self.create_snake()
         self.head = self.segments[0]
+
+    def move(self):
+        for seg in range(len(self.segments) -1, 0, -1):
+            new_x = self.segments[seg -1].xcor()
+            # gives to the last segment the position of the segment before him
+            new_y = self.segments[seg -1].ycor()
+            self.segments[seg].goto(new_x, new_y)
+        self.head.forward(MOVE)
+
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
