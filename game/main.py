2336bd878fb3442d2bbf385109e8f31166fdece8
Adrian <adrianqueizan@gmail.com>
2024-02-20 11:30:18 -0500

    created snake

--- game/main.py
+++ game/main.py
@@ -1,9 +1,12 @@
 from turtle import Screen
+from snake import Snake
 
 screen = Screen()
 screen.setup(800, 600)
 screen.bgcolor('black')
 screen.title('Snake Game')
+
+snake = Snake()
 
 game_on = True
 while game_on:
---
+++ game/snake.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+POSITIONS = [(0,0), (-20,0), (-40,0)]
+MOVE = 20
+
+class Snake:
+    
+
+    def __init__(self) -> None:
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        segment = Turtle('square')
+        segment.color('white')
+        segment.penup()
+        segment.goto(position)
+        self.segments.append(segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
